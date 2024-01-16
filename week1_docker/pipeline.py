8971b21b815ba8ed61f42481cddc52eb1bbf00fc
Natali-bali <ko.natalia.de@gmail.com>
2024-01-16 19:27:45 +0000

    first commit

---
+++ week1_docker/pipeline.py
@@ -0,0 +1,8 @@
+import pandas as pandas
+#to add parameters from bash string
+#0-name of the file, 1-what we added 
+import sys
+day = sys.argv[1]
+# some fancy staff
+
+print(f'Job finished successfully!!! for day = {day}')
